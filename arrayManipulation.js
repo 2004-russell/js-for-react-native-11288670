16cf0260f78ecce1a699ae8824eba04eeccb83db
2004-russell <151689516+2004-russell@users.noreply.github.com>
2024-05-18 15:39:58 -0700

    Add files via upload

---
+++ arrayManipulation.js
@@ -0,0 +1,26 @@
+// arrayManipulation.js
+// Task 1
+function processArray(numbers) {
+    return numbers.map(number => {
+        if (number % 2 === 0) {
+            return number * number; 
+        } else {
+            return number * 3; 
+        }
+    });
+}
+
+
+function formatArrayStrings(strings, numbers) {
+    return strings.map((str, index) => {
+        if (numbers[index] % 2 === 0) {
+            return str.toUpperCase(); 
+        } else {
+            return str.toLowerCase(); 
+        }
+    });
+}
+module.exports = {
+    processArray,
+    formatArrayStrings
+};
